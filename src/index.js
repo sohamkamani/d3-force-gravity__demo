aa68c03fe81a45ba0eab803d7b3867f121f38797
sohamkamani <sohamkamani@gmail.com>
2016-08-12 18:07:07 +0530

    removing drag

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const d3 = require('d3')
-const forceG = require('./gravity')
+const forceG = require('d3-force-gravity')
 
 const canvas = document.querySelector('canvas')
 const {width, height} = canvas
 
-const nodes = d3.range(1).map(i => ({
+const nodes = d3.range(40).map(i => ({
 	index: i,
 	x: width / 2,
 	y: 0,
@@ -17,19 +17,9 @@ const tau = 2 * Math.PI
 const render = () => {
 	context.clearRect(0, 0, width, height)
 	context.save()
-	// context.translate(width / 2, height / 2)
 
 	context.beginPath()
 	nodes.forEach(function(d) {
-		// if (d.y >= height - 5) {
-		// 	d.y = height - 5
-		// }
-		// if (d.x <= 5) {
-		// 	d.x = 5
-		// }
-		// if (d.x >= width) {
-		// 	d.x = width
-		// }
 		context.moveTo(d.x + d.r, d.y)
 		context.arc(d.x, d.y, d.r, 0, tau)
 	})
@@ -43,38 +33,34 @@ const render = () => {
 
 const simulation = d3.forceSimulation()
 	.nodes(nodes)
-	// .force('gravity', d3.forceCenter(width / 2, 40))
-	// .force('x1', d3.forceX(width / 2).strength(0.02))
-	// .force('y', d3.forceY(height + 100).strength(0.02))
-	// .force('y', d3.forceY(0).strength(0.02))
-	.force('g', forceG(width / 2, height / 2).minRadius(30).strength(10000))
+	.force('g', forceG(width / 2, height / 2)
+		.minRadius(50)
+		.strength(10000))
 	.force('collide', d3.forceCollide().radius(() => 7).iterations(2))
 	.alphaTarget(0.4)
 	.on('tick', render)
-
-const dragsubject = () => simulation.find(d3.event.x, d3.event.y, 5)
-
-d3.select(canvas)
-	.call(d3.drag()
-		.container(canvas)
-		.subject(dragsubject)
-		.on('start', dragstarted)
-		.on('drag', dragged)
-		.on('end', dragended))
-
-function dragstarted() {
-	// if (!d3.event.active) simulation.alphaTarget(0.3).restart()
-	d3.event.subject.fx = d3.event.subject.x
-	d3.event.subject.fy = d3.event.subject.y
-}
-
-function dragged() {
-	d3.event.subject.fx = d3.event.x
-	d3.event.subject.fy = d3.event.y
-}
-
-function dragended() {
-	// if (!d3.event.active) simulation.alphaTarget(0)
-	d3.event.subject.fx = null
-	d3.event.subject.fy = null
-}
+	// 
+	// const dragsubject = () => simulation.find(d3.event.x, d3.event.y, 5)
+	//
+	// d3.select(canvas)
+	// 	.call(d3.drag()
+	// 		.container(canvas)
+	// 		.subject(dragsubject)
+	// 		.on('start', dragstarted)
+	// 		.on('drag', dragged)
+	// 		.on('end', dragended))
+	//
+	// function dragstarted() {
+	// 	d3.event.subject.fx = d3.event.subject.x
+	// 	d3.event.subject.fy = d3.event.subject.y
+	// }
+	//
+	// function dragged() {
+	// 	d3.event.subject.fx = d3.event.x
+	// 	d3.event.subject.fy = d3.event.y
+	// }
+	//
+	// function dragended() {
+	// 	d3.event.subject.fx = null
+	// 	d3.event.subject.fy = null
+	// }
